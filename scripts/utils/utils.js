87c83728cc8ade78204697daf6c040d91c615013
Josh Winn <965114+jawinn@users.noreply.github.com>
2026-08-20 11:22:46 -0400

    feat: utils file and keep datastore from adobe design

---
+++ scripts/utils/utils.js
@@ -0,0 +1,6 @@
+/**
+ * Utilities / helpers used by blocks and custom scripts.
+ *
+ * Note: Because there is no bundler, avoid importing files here, so
+ * there are not an excessive number of network requests on the page.
+ */
